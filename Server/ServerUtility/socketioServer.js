8b6f168c259be27cc7f33ab550e7bc542cbb26ae
nuno <nuno-costa17@hotmail.com>
2025-01-17 23:18:15 +0000

    docs, UPDATES

--- Server/ServerUtility/socketioServer.js
+++ Server/ServerUtility/socketioServer.js
@@ -1,38 +1,25 @@
 import { Server as SocketIO } from "socket.io";
 
-// const FRONTEND_URL =
-//   process.env.VITE_FRONTEND_URL ||
-//   (process.env.NODE_ENV === "production"
-//     ? "https://my-gym-app-client.vercel.app"
-//     : "http://localhost:5173");
-
 const allowedOrigins = [
   "https://my-gym-app-client.vercel.app",
   "https://my-gym-app-client-6e0oo4ktu-costa404s-projects.vercel.app",
 ];
 
 const createSocketServer = (httpServer) => {
-  const allowedOrigins = [
-    "https://my-gym-app-client.vercel.app",
-    "https://my-gym-app-client-6e0oo4ktu-costa404s-projects.vercel.app",
-  ];
-
-  const createSocketServer = (httpServer) => {
-    const io = new SocketIO(httpServer, {
-      cors: {
-        origin: (origin, callback) => {
-          if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true); // ✅ Permite a conexão
-          } else {
-            callback(new Error("Not allowed by CORS")); // ❌ Bloqueia
-          }
-        },
-        methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
+  const io = new SocketIO(httpServer, {
+    cors: {
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS")); // ❌ Bloqueia
+        }
       },
-    });
-  };
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  });
 
   io.on("connection", (socket) => {
     console.log("Novo cliente conectado", socket.id);
@@ -52,7 +39,6 @@ const createSocketServer = (httpServer) => {
         }
 
         console.log("workoutId", workoutId);
-
         socket.emit("exerciseStatsSaved", { success: true });
       } catch (error) {
         console.error("Erro ao salvar os dados de exercício:", error);
